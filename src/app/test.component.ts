244cc8136d7693535052b9c5dac0d912f1c8d74a
Srinivas <srinivas.gundali@accenture.com>
2024-08-01 18:59:08 +0800

    conditional rendering

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -3,12 +3,17 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
+    @if (isUserNameSet) {
     Username: {{ username }}
+    } @else {
+    Username : 'Guest'
+    }
   `,
   standalone: true,
 })
 export class UserComponent {
   username = 'youngTech';
+  isUserNameSet = true;
 }
 
 @Component({
