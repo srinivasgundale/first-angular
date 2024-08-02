2441bdc02c1067eeba124b1e02bb65e2abe4a8d0
Srinivas <srinivas.gundali@accenture.com>
2024-08-02 12:06:46 +0800

    Pipes and custom pipes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import {FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from '../services/user.service';
+import {UpperCasePipe, LowerCasePipe } from '@angular/common';
+import { StarPipe } from '../pipes/star.pipe';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, UpperCasePipe, LowerCasePipe, StarPipe],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
@@ -12,7 +14,7 @@ export class UserComponent {
   display = ''
   displayTwo = ''
   constructor(private us: UserService) {
-    this.display = this.userService.getCars().join(' ⭐️ ');
+    this.display = this.userService.getCars().join(' ⭐️ ') ;
     this.displayTwo = this.us.getCars().join(' ⭐️ ');
   }
   favoriteFramework = '';
