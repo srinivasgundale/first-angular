19a4179a5f4712394aa62d453fae92a958b29ca0
Srinivas <srinivas.gundali@accenture.com>
2024-08-02 11:16:29 +0800

    Form, reactive form, validator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import {FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  favoriteFramework = '';
+  name=''
+  showFramework() {
+    alert(this.favoriteFramework);
+  }
+  showName() {
+    alert(this.name);
+  }
+  showForm() {
+    alert(this.favoriteFramework+" "+this.name);
+  }
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+  handleSubmit() {
+    alert(
+      this.profileForm.value.name + ' | ' + this.profileForm.value.email
+    );
+  }
 }
+
+
